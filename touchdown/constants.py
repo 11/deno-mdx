69a02655d29d39fea090c9fc95d6507f034b5081
Doug Rudolph <drudolph914@gmail.com>
2022-09-01 16:10:48 -0400

    updating code block regex -- adding math block regesx for future commit

--- touchdown/constants.py
+++ touchdown/constants.py
@@ -2,8 +2,8 @@ MARKDOWN_REGEXS = {
     # https://www.debuggex.com/r/yJLwFDiFjDuifTSr
     'header': r'^(#{1,6}?) (.*?)$',
 
-    # https://www.debuggex.com/r/4xeF1p18gQjjhAe9
-    'blockquote': r'^\> [\s\S]*$',
+    # https://www.debuggex.com/r/CtnsXDb7jI1pMumX
+    'blockquote': r'^\> [\s]*(.*)$',
 
     # https://www.debuggex.com/r/s2BZUI9PedP0m5O-
     'ordered_list': r'^\s*[0-9]{1,}. ([\s\S]*)\n$',
@@ -17,8 +17,14 @@ MARKDOWN_REGEXS = {
     # https://www.debuggex.com/r/dnmmbV9HXMOBFQPa
     'link': r'^\[(.*)\]\((.*)\)$',
 
-    # https://www.debuggex.com/r/7D4R7b9LVt8QbJ1l
-    'codeblock': r'^```$',
+    # https://www.debuggex.com/r/dB-xS-Jo6RGl9NW2
+    'codeblock_header': r'^(```)[\s]*?([a-zA-Z1-9]*)[\s]*?[\n\r]',
+
+    # https://www.debuggex.com/r/hEMpYtUnAaOJO-At
+    'codeblock_footer': r'^```[\n\r]*?',
+
+    # TODO: need to add support for latex math syntax
+    'math': r'r\$\$$',
 }
 
 
@@ -28,6 +34,3 @@ SPECIAL_CHARS = set([
     '~', # strikethrough
     '`', # code
 ])
-
-
-
