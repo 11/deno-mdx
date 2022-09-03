29d7d0154194b4c7137d3a7eb62710aa94132bf3
Doug Rudolph <drudolph914@gmail.com>
2022-09-03 03:07:09 -0400

    adding first attempt at setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('README.md', r) as f:
+    long_description = f.read()
+
+setup(name='Distutils',
+    version='1.0',
+    description='Mdx compiler and CLI',
+    long_description=long_description,
+    author='Doug Rudolph',
+    url='https://github.com/11/touchdown',
+    packages=['touchdown'],
+ )
