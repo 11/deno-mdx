f237ccfb124b1802047834fbaaa15c103c730a28
Doug Rudolph <drudolph914@gmail.com>
2022-09-18 16:58:07 -0400

    wrote all tests for ordered list

--- tests/test_ordered_list.py
+++ tests/test_ordered_list.py
@@ -4,38 +4,377 @@ from pathlib import Path
 from touchdown import markdown, html
 
 
-TESTCASE_DIR = '../testcases/list/ordered'
+TESTCASE_DIR = './testcases/list/ordered'
 
 
 class TestOrderedList(unittest.TestCase):
     def test_list_element_numbers_are_corrected_markdown(self):
+        """ test that the list element number is normalized """
+
         test_file = Path(f'{TESTCASE_DIR}/test_list_element_numbers_are_corrected.md')
-        assert False
+        expected_markdown = {
+            "content": [
+                {
+                    "content": [
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "Lorem ipsum " "dolor sit amet",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "consectetur " "adipiscing " "elit",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "sed do eiusmod " "tempor " "incididunt ",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "ut labore et " "dolore magna " "aliqua.",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "the list "
+                                        "elmeents "
+                                        "should be "
+                                        "corrected to "
+                                        "be 1,2,3,4,5",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                    ],
+                    "tag": "ol",
+                    "type": "ordered_list",
+                }
+            ],
+            "filename": "test_list_element_numbers_are_corrected.md",
+        }
+
+        assert markdown(test_file) == expected_markdown
 
     def test_list_element_numbers_are_corrected_html(self):
+        """ test that the list element number is normalized """
+
         test_file = Path(f'{TESTCASE_DIR}/test_list_element_numbers_are_corrected.md')
-        assert False
+        expected_html = \
+            '<ol>\n' \
+            '\t<li>Lorem ipsum dolor sit amet</li>\n' \
+            '\t<li>consectetur adipiscing elit</li>\n' \
+            '\t<li>sed do eiusmod tempor incididunt </li>\n' \
+            '\t<li>ut labore et dolore magna aliqua.</li>\n' \
+            '\t<li>the list elmeents should be corrected to be 1,2,3,4,5</li>\n' \
+            '</ol>'
+ 
+        assert html(test_file) == expected_html
 
     def test_several_list_elements_markdown(self):
+        """ test that a single orderd list will append multiple list elements """
+
         test_file = Path(f'{TESTCASE_DIR}/test_several_list_elements.md')
-        assert False
+        expected_markdown = {
+            "content": [
+                {
+                    "content": [
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "Lorem ipsum " "dolor sit amet",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "consectetur " "adipiscing " "elit",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "sed do eiusmod " "tempor " "incididunt ",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "ut labore et " "dolore magna " "aliqua.",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                    ],
+                    "tag": "ol",
+                    "type": "ordered_list",
+                }
+            ],
+            "filename": "test_several_list_elements.md",
+        }
+ 
+        assert markdown(test_file) == expected_markdown
 
     def test_several_list_elements_html(self):
+        """ test that a single orderd list will append multiple list elements """
+
         test_file = Path(f'{TESTCASE_DIR}/test_several_list_elements.md')
-        assert False
+        expected_html = \
+            '<ol>\n' \
+            '\t<li>Lorem ipsum dolor sit amet</li>\n' \
+            '\t<li>consectetur adipiscing elit</li>\n' \
+            '\t<li>sed do eiusmod tempor incididunt </li>\n' \
+            '\t<li>ut labore et dolore magna aliqua.</li>\n' \
+            '</ol>'
+ 
+        assert html(test_file) == expected_html
 
     def test_several_lists_markdown(self):
+        """ test that list elements are correctly appended to their respective list """
+
         test_file = Path(f'{TESTCASE_DIR}/test_several_lists.md')
-        assert False
+        expected_markdown = {
+            "content": [
+                {
+                    "content": [
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "Lorem ipsum " "dolor sit amet",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "consectetur "
+                                        "adipiscing "
+                                        "elit sed do "
+                                        "eiusmod tempor "
+                                        "incididunt ut "
+                                        "labore et "
+                                        "dolore magna "
+                                        "aliqua",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                    ],
+                    "tag": "ol",
+                    "type": "ordered_list",
+                },
+                {
+                    "content": [
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "Ut enim ad " "minim veniam",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {"content": "quis nostrud ", "tag": None, "type": None}
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                        {
+                            "content": {
+                                "content": [
+                                    {"content": "exercitation ", "tag": None, "type": None}
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        },
+                    ],
+                    "tag": "ol",
+                    "type": "ordered_list",
+                },
+                {
+                    "content": [
+                        {
+                            "content": {
+                                "content": [
+                                    {
+                                        "content": "ullamco "
+                                        "laboris nisi "
+                                        "ut aliquip ex "
+                                        "ea commodo "
+                                        "consequat",
+                                        "tag": None,
+                                        "type": None,
+                                    }
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        }
+                    ],
+                    "tag": "ol",
+                    "type": "ordered_list",
+                },
+            ],
+            "filename": "test_several_lists.md",
+        }
+
+        assert markdown(test_file) == expected_markdown
 
     def test_several_lists_html(self):
+        """ test that list elements are correctly appended to their respective list """
+
         test_file = Path(f'{TESTCASE_DIR}/test_several_lists.md')
-        assert False
+        expected_html = \
+            '<ol>\n' \
+            '\t<li>Lorem ipsum dolor sit amet</li>\n' \
+            '\t<li>consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>\n' \
+            '</ol>\n' \
+            '<ol>\n' \
+            '\t<li>Ut enim ad minim veniam</li>\n' \
+            '\t<li>quis nostrud </li>\n' \
+            '\t<li>exercitation </li>\n' \
+            '</ol>\n' \
+            '<ol>\n' \
+            '\t<li>ullamco laboris nisi ut aliquip ex ea commodo consequat</li>\n' \
+            '</ol>'
 
-    def test_single_list_elemment_markdown(self):
-        test_file = Path(f'{TESTCASE_DIR}/test_single_list_elemment.md')
-        assert False
+        assert html(test_file) == expected_html
 
-    def test_single_list_elemment_html(self):
-        test_file = Path(f'{TESTCASE_DIR}/test_single_list_elemment.md')
-        assert False
+    def test_single_list_element_markdown(self):
+        """ sanity test for creating a ordered list with 1 list element """
+
+        test_file = Path(f'{TESTCASE_DIR}/test_single_list_element.md')
+        expected_markdown = {
+            "content": [
+                {
+                    "content": [
+                        {
+                            "content": {
+                                "content": [
+                                    {"content": "this is a test", "tag": None, "type": None}
+                                ],
+                                "type": "text",
+                            },
+                            "tag": "li",
+                            "type": "listitem",
+                        }
+                    ],
+                    "tag": "ol",
+                    "type": "ordered_list",
+                }
+            ],
+            "filename": "test_single_list_element.md",
+        }
+
+        assert markdown(test_file) == expected_markdown
+
+    def test_single_list_element_html(self):
+        """ sanity test for creating a ordered list with 1 list element """
+
+        test_file = Path(f'{TESTCASE_DIR}/test_single_list_element.md')
+        expected_html = \
+            '<ol>\n' \
+            '\t<li>this is a test</li>\n' \
+            '</ol>'
+
+        assert html(test_file) == expected_html
